8164afa789912dfc29bd50668fa50947ee6cab04
Hagan Barber <haganb3@gmail.com>
2018-07-13 11:37:30 -0500

    modified footer to include travis county map as a background image from backgroundImg folder. Created backgroundImg folder with travis county map inside

--- footer.php
+++ footer.php
@@ -1,16 +1,15 @@
-<?php
-      $attachmentID = 1538;
-      $imageSizeName = "full";
-      $img = wp_get_attachment_image_src($attachmentID, $imageSizeName);
-      //print_r($img);
-?>
+<!-- <?php
+    //   $attachmentID = 1538;
+    //   $imageSizeName = "full";
+    //   $img = wp_get_attachment_image_src($attachmentID, $imageSizeName);
+?> -->
 
-
+<!-- <?php echo $img[0]; ?> -->
 <footer>
 	<section class="container-fluid newsletter" style="background: linear-gradient(
 	        rgba(45, 49, 70, 0.85),
 	        rgba(45, 49, 70, 0.85)),
-    		url(<?php echo $img[0]; ?>) 50% 30% no-repeat; background-size: cover;" >
+    		url('./wp-content/themes/archives/backgroundImg/travis_county-1932-map-bw.jpg') 50% 30% no-repeat; background-size: cover;" >
 		<div class="container newsletter-container">
     		<div class="row">
 			<?php if ( dynamic_sidebar( 'newsletter' ) ); ?>
