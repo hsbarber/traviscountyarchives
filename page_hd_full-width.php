eefa6f5d8a2cddff5c192c7538c09b238fa4bb52
Hagan Barber <haganb3@gmail.com>
2020-10-13 17:19:46 -0500

    worked on intro page for retrospective.New full width page for individual hd pages. Fixed timeline and made separate stylesheet for it

---
+++ page_hd_full-width.php
@@ -0,0 +1,48 @@
+<?php
+/*
+  Template Name: History Day Full Width Template
+
+ */
+?>
+<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+<?php get_header(); ?>
+<div class="container-fluid bc-container">
+  <div class="row">
+    <div class="container">
+     <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+        <?php if(function_exists('bcn_display'))
+        {
+            bcn_display();
+        }?>
+     </div>
+    </div>
+  </div>
+</div>
+<div class="hd-page-bg" style="background: url('<?php echo $backgroundImg[0]; ?>') 50% 50% no-repeat; background-size: cover; background-attachment: fixed;">
+
+    <div class="container hd-page-container">
+        <div class="row">
+
+            <div class="col-md-12 page-container-full-width">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                <div class="hd-header">
+                    <h2><?php the_title(); ?></h2>
+                </div>
+                <div class="the_content">
+                    <?php the_content(); ?>
+                </div>
+                <?php endwhile; else: ?>
+                    <div class="page-header">
+                        <h1>Oh no!</h1>
+                    </div>
+                    <p>No content is appearing for this page!</p>
+                    <?php endif; ?>
+            </div>
+            <!--end page__container#### -->
+
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
+
--- page_timeline_full-width.php
+++ page_timeline_full-width.php
@@ -4,6 +4,7 @@
 
  */
 ?>
+<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 <?php get_header(); ?>
 <div class="container-fluid bc-container">
   <div class="row">
@@ -17,32 +18,34 @@
     </div>
   </div>
 </div>
-<div class="container">
-  <div class="row">
+<div class="intro-page-bg" style="background: url('<?php echo $backgroundImg[0]; ?>') 50% 50% no-repeat; background-size: cover;">
+    <h1><?php the_title(); ?></h1>
+</div>
+    <div class="container hd-page-container">
+        <div class="row">
 
-        <div class="col-md-12 page-container-full-width">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <div class="col-md-12 page-container-full-width">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <div class="page-header">
-                <h2><?php the_title(); ?></h2>
-            </div>
-            <div class="the_content">
-                <?php the_content(); ?>
-            </div>
-            <!-- TIMELINE SECTION -->
-            <section class="timeline">
+
+                    <div class="the_content hd-content">
+                        <?php the_content(); ?>
+                    </div>
+
+                <!-- TIMELINE SECTION -->
+                <section class="hd-timeline">
                 <?php
 
-                $fields = get_fields();
+                    $fields = get_fields();
 
                 if( $fields ): ?>
-                <div class="timeline__container">
+                <div class="hd-timeline__container">
                     <?php foreach( $fields as $name => $value ): ?>
-                        <div class="timeline__block js-block">
-                            <div class="timeline__content js-content">
-                                    <div class="timeline__img js-img">
+                        <div class="hd-timeline__block js-block">
+                            <div class="hd-timeline__content js-content">
+                                    <div class="hd-timeline__img js-img">
                                     </div> <!-- cd-timeline__img -->
-                                    <?php echo $value; ?>
+                                        <?php echo $value; ?>
 
                             </div> <!-- end timeline__content -->
                         </div> <!-- end timeline__block -->
@@ -50,16 +53,17 @@
                 </div> <!--end timeline__container -->
                 <?php endif; ?>
             </section>
-            <?php endwhile; else: ?>
-                <div class="page-header">
-                    <h1>Oh no!</h1>
-                </div>
-                <p>No content is appearing for this page!</p>
-                <?php endif; ?>
-        </div>
-        <!--end page__container#### -->
+                <?php endwhile; else: ?>
+                    <div class="page-header">
+                        <h1>Oh no!</h1>
+                    </div>
+                    <p>No content is appearing for this page!</p>
+                    <?php endif; ?>
+            </div>
+            <!--end page__container#### -->
 
-      </div>
-</div>
+        </div>
+    </div>
+
 <?php get_footer(); ?>
 
